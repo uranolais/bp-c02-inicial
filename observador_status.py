a0cf71b581868d5b47b4e8e60568bbada63bca8a
Laís Urano <lais.urano@gmail.com>
2024-11-13 17:33:25 -0300

    i

---
+++ observador_status.py
@@ -0,0 +1,13 @@
+class ObservadorStatus:
+    def __init__(self):
+        self.observadores = []
+
+    def adicionar_observador(self, observador):
+        self.observadores.append(observador)
+
+    def remover_observador(self, observador):
+        self.observadores.remove(observador)
+
+    def notificar_observadores(self, mensagem):
+        for observador in self.observadores:
+            observador.enviar(mensagem)
--- pedido/pedido.py
+++ pedido/pedido.py
@@ -1,12 +1,14 @@
 from abc import ABC, abstractmethod
 from item import Item
 from cliente import Cliente
+from observador_status import ObservadorStatus
 
 class Pedido(ABC):
     def __init__(self, cliente, itens):
         self.cliente = cliente
         self.itens = itens
         self.status = "Novo"
+        self.observador_status = ObservadorStatus()
     
     @abstractmethod
     def calcular_total(self):
@@ -14,3 +16,10 @@ class Pedido(ABC):
 
     def enviar_notificacao(self, notificacao, mensagem):
         notificacao.enviar(self.cliente, mensagem)
+    
+    def adicionar_observador(self, observador):
+        self.observador_status.adicionar_observador(observador)
+
+    def atualizar_status(self, novo_status):
+        self.status = novo_status
+        self.observador_status.notificar_observadores(f"Status atualizado para: {novo_status}")
